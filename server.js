6e5d1793dea0469723ecba3a28ffeea9af417bff
Jens Ackou <shackofnoreturn@gmail.com>
2023-08-16 22:24:32 +0200

    Code cleanup

--- server.js
+++ server.js
@@ -1,31 +1,32 @@
-const express = require("express");
+const express    = require("express");
 const bodyParser = require('body-parser');
+
+// Defining constants
 const app  = express();
+const port = 3000;
+
+// Application configuration
 app.use(bodyParser.json({ limit: '10mb' }));
-// Set up static file serving
 app.use(express.static('public'));
 
+// Upload endpoint
 app.post("/upload", (req, res) => {
-    console.log(`Upload endpoint reached.`);
-    
     const img = req.body.file;
-     var regex = /^data:.+\/(.+);base64,(.*)$/;
+    var regex = /^data:.+\/(.+);base64,(.*)$/;
 
-     var matches = string.match(regex);
-     var ext = matches[1];
-     var data = matches[2];
-     var buffer = Buffer.from(data, 'base64'); //file buffer
+    var matches = string.match(regex);
+    var ext = matches[1];
+    var data = matches[2];
+    var buffer = Buffer.from(data, 'base64'); //file buffer
       
-     //do whatever you want with the buffer
-
      fs.writeFileSync('imagename.jpg' + ext, buffer); //if you do not need to save to file, you can skip this step.
      
-     // return res to client
+     // return response to client
      res.send('String received successfully');
 });
 
 // Start server
-var server = app.listen(3000, function () {
+var server = app.listen(port, function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('listening at http://%s:%s', host, port);
