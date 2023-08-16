2e7cc8e6b519fe66af8b2334e9d14c1a642f5773
Jens Ackou <shackofnoreturn@gmail.com>
2023-08-16 22:14:14 +0200

    Creating new picture capture and sending functionality

--- public/app.js
+++ public/app.js
@@ -29,6 +29,25 @@ cameraTrigger.addEventListener('click', async () => {
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
+    
+    const canvas = document.createElement('canvas');
+    canvas.width = cameraView.videoWidth;
+    canvas.height = cameraView.videoHeight;
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(cameraView, 0, 0, canvas.width, canvas.height);
+    const imageData = canvas.toDataURL('image/jpeg');
+
+    const response = await fetch('/upload', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ image: imageData })
+    });
+
+    if (response.ok) {
+        console.log('Photo sent successfully!');
+    } else {
+        console.error('Failed to send photo.');
+    }
 });
 
 // Start camera when page loads
